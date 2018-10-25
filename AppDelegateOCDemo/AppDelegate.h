2d44094e43e95cbc673583faf1772aa3dccf0aa3
yp liao <251180323@qq.com>
2018-10-25 16:32:30 +0800

    Initial Commit

---
+++ AppDelegateOCDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AppDelegateOCDemo
+//
+//  Created by larry on 2018/10/25.
+//  Copyright © 2018 liaoyp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AppDelegateOCDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AppDelegateOCDemo
+//
+//  Created by larry on 2018/10/25.
+//  Copyright © 2018 liaoyp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
